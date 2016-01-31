653ca1a619a4c47e179ef09587a12baf9d6d7703
Chin Ting Chan <chintingchan@gmail.com>
2016-01-30 21:20:34 -0600

    added auto-indent with vim

--- GlobalVariables.cpp
+++ GlobalVariables.cpp
@@ -2,4 +2,4 @@
 
 const int MAXINT = 2147483647; // max number an integer can hold
 const string TABLE[12] = { "C  ", "C# ", "D  ", "Eb ", "E  ", "F  ", // chromatic table for printing
-						   "F# ", "G  ", "Ab ", "A  ", "Bb ", "B  " };
+    "F# ", "G  ", "Ab ", "A  ", "Bb ", "B  " };
--- main.cpp
+++ main.cpp
@@ -4,26 +4,26 @@
 #include "Modules.h"
 
 int main() {
-	heading();
+    heading();
 
-	const int NUMBER_OF_PROGRAMS = 13;   // number of sub-programs that can be called
-	int menuChoice;
-	bool valid;	// input valid check
+    const int NUMBER_OF_PROGRAMS = 13;   // number of sub-programs that can be called
+    int menuChoice;
+    bool valid;	// input valid check
 
-	do {
-		valid = true;   // initialize valid to be true;
-		menu(); // print out menu
-		cin >> menuChoice;
-		cout << endl;
-		programCalling(menuChoice); // call programs
-		if (menuChoice <= NUMBER_OF_PROGRAMS && menuChoice > 0)
-			// only print this line if user's menu choice has been within the correct range
-			// and when the program has been executed corrected
-			cout << endl << "What would you like to do next?" << endl;
-	} while (valid == false || menuChoice != 0);
-	// valid is a global variable it is changed inside programCalling()
+    do {
+        valid = true;   // initialize valid to be true;
+        menu(); // print out menu
+        cin >> menuChoice;
+        cout << endl;
+        programCalling(menuChoice); // call programs
+        if (menuChoice <= NUMBER_OF_PROGRAMS && menuChoice > 0)
+            // only print this line if user's menu choice has been within the correct range
+            // and when the program has been executed corrected
+            cout << endl << "What would you like to do next?" << endl;
+    } while (valid == false || menuChoice != 0);
+    // valid is a global variable it is changed inside programCalling()
 
-	cout << "Exiting the program..." << endl; // end of program
+    cout << "Exiting the program..." << endl; // end of program
 
-	return 0;
+    return 0;
 }
